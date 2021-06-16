45ab767a4ed9ef2858c7746cc6e9c727d7aa18d3
Timothy <44120806+Timmy526@users.noreply.github.com>
2021-06-16 15:28:59 -0400

    Add files via upload
    Editing Landing Page further

---
+++ script.js
@@ -0,0 +1,25 @@
+//NavBar Script
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+
+//Quote of the Day Script
+let request = new XMLHttpRequest();
+let url = "https://api.adviceslip.com/advice";
+
+request.open("GET", url, true);
+
+request.onload = function(){
+    //Begin accessing JSON data here. Data stored in request.response
+    let data = JSON.parse(this.response);
+    let quote = document.getElementById('quote');
+    
+    if (request.status >= 200 && request.status <400){
+        quote.innerHTML = data["slip"].advice;
+    }
+    
+};
+request.send();
